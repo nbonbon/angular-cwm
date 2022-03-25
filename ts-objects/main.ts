efb7e7b8ce244faf4c96fa4d5b72e5468167420f
Nick Bonnet <njbuwm@gmail.com>
2022-03-25 09:41:18 -0400

    ts-objects

---
+++ ts-objects/main.ts
@@ -0,0 +1,19 @@
+// object
+class Point {
+    x: number; 
+    y: number; 
+    
+    draw() {
+        console.log('x: ' + this.x + ', Y: ' + this.y);
+    }
+
+    getDistance(another: Point) {
+
+    }
+}
+
+// instance
+let point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
