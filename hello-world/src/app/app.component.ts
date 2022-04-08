aa87efc686d165999c1b20ca1f7392d3b51f0b90
Nick Bonnet <njbuwm@gmail.com>
2022-04-08 07:24:55 -0400

    Passing Event Data - interface Object

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FavoriteChangedEventArgs } from './favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
     isFavorite: true
   }
 
-  onFavoriteChanged(isFavorite: boolean) {
-    console.log("Favorite changed: ", isFavorite);
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
+    console.log("Favorite changed: ", eventArgs);
   }
 }
--- hello-world/src/app/favorite.component.ts
+++ hello-world/src/app/favorite.component.ts
@@ -16,6 +16,12 @@ export class FavoriteComponent implements OnInit {
 
   onClick() { 
     this.isFavorite = !this.isFavorite;
-    this.change.emit(this.isFavorite);
+    this.change.emit({
+      newValue : this.isFavorite
+    });
   }
+}
+
+export interface FavoriteChangedEventArgs {
+  newValue: boolean
 }
