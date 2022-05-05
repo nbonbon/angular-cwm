fb06d03537ec393d39a8d71eb6f9edb9bf66b56b
Nick Bonnet <njbuwm@gmail.com>
2022-05-05 10:19:42 -0400

    Custom Directives - Single input property with custom directive

--- directives/src/app/input-format.directive.ts
+++ directives/src/app/input-format.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  @Input('format') format: string = '';
+  @Input('appInputFormat') format: string = '';
   
   constructor(private el: ElementRef) { }
   
