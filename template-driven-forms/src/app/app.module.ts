2edea1bc0712c4dfd01a906e1f254bb331756fe5
Nick Bonnet <njbuwm@gmail.com>
2022-05-27 20:22:29 -0400

    Template Driven Forms - New Course Form Exercise

--- template-driven-forms/src/app/app.module.ts
+++ template-driven-forms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
