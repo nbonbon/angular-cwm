771f855f90a340468b249eeaee38fa21e375d66c
Nick Bonnet <njbuwm@gmail.com>
2022-07-25 14:46:04 -0400

    Deleting Data

--- consuming-http-services/src/app/posts/posts.component.ts
+++ consuming-http-services/src/app/posts/posts.component.ts
@@ -30,4 +30,15 @@ export class PostsComponent {
       }
     );
   }
+
+  updatePost(post: any) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      } 
+    );
+
+    // PUT
+    //this.http.put(this.url, JSON.stringify(post));
+  }
 }
