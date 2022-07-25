2f9922aa2b66dbe70cccc21e7617a895acf1a0ce
Nick Bonnet <njbuwm@gmail.com>
2022-07-25 14:38:16 -0400

    Creating Data

--- consuming-http-services/src/app/posts/posts.component.ts
+++ consuming-http-services/src/app/posts/posts.component.ts
@@ -8,11 +8,26 @@ import { Component } from '@angular/core';
 })
 export class PostsComponent {
   posts: any = [];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) {
+    this.http.get(this.url)
       .subscribe(response => {
           this.posts = response;
-      });
+      }
+    );
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = { title: input.value };
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = (response as any)['id'];
+        this.posts.splice(0, 0, post);
+        console.log(response);
+      }
+    );
   }
 }
